fb2d692557b444c394e720955b3438033204d931
Zhou Hongyu <zhouhongyucs@163.com>
2025-12-30 22:56:32 +0800

    Enhance language toggle with updated translations
    Updated the introduction text and language toggle functionality to include 'Embodied AI' in the English translation.

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const languageToggle = document.getElementById('language-toggle');
     
     const translations = {
-        'en': "Hello, this is Zhou Hongyu. I am currently focused on learning and exploring fields such as Artificial Intelligence Infrastructure (AI Infra) and Robotics. During my undergraduate studies, I focused on Cognitive Science, which effectively cultivated a holistic mindset for understanding and reshaping the world.",
-        'zh': "你好，这里是Zhou Hongyu，目前专注于人工智能基础设施（AI Infra）、机器人科学（Robotics）等领域的学习与探索。在本科阶段，我着重关注认知科学，这很好地为我认识、改造世界构建了全局性思维。"
+        'en': "Hello, this is Zhou Hongyu. I am currently focused on learning and exploring fields such as AI Infra and Embodied AI. During my undergraduate studies, I focused on Cognitive Science, which effectively cultivated a holistic mindset for understanding and reshaping the world.",
+        'zh': "你好，这里是Zhou Hongyu，目前专注于人工智能基础设施（AI Infra）、具身智能（Embodied AI）等领域的学习与探索。在本科阶段，我着重关注认知科学，这很好地为我认识、改造世界构建了全局性思维。"
     };
     
     // 初始状态为中文
@@ -26,3 +26,4 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 });
+
